107dce9a096bdf79b5dab03fa2e0fe446b6e23fc
FERGAZA <fernandomgzapata@gmail.com>
2021-07-28 21:13:37 +0000

    Fix room id bug

--- utils/sockets/queue.js
+++ utils/sockets/queue.js
@@ -1,17 +1,26 @@
 const { Room } = require('../../models')
 
-module.exports = (socket, io) => socket.on('modify-queue', data => {
-  const { sockets } = io
-  const { queue } = data
-  const { id } = data 
-  const { isSkiping } = data
-  const room = { queue: queue && typeof queue === 'string' ? replaceAll(queue, ' ').split(',') : queue || [] }
-
-  Room.updateOne({ _id: id }, room, (err, res) => {
-    if (err) catchErr(err, res)
-    else {
-      sockets.emit('send-queue', queue)
-      if (isSkiping) sockets.emit('skip', 'skip!')
-    }
+module.exports = (socket, io) => {
+  socket.on('modify-queue', data => {
+    const { sockets } = io
+    const { queue } = data
+    const { id } = data 
+    const room = { queue: queue && typeof queue === 'string' ? replaceAll(queue, ' ').split(',') : queue || [] }
+  
+    Room.updateOne({ _id: id }, room, (err, res) => {
+      if (err) catchErr(err, res)
+      else sockets.emit('send-queue', { queue, id })
+    })
   })
-})
+  socket.on('add-queue', data => {
+    const { sockets } = io
+    const { queue } = data
+    const { id } = data 
+    const room = { queue: queue && typeof queue === 'string' ? replaceAll(queue, ' ').split(',') : queue || [] }
+  
+    Room.updateOne({ _id: id }, room, (err, res) => {
+      if (err) catchErr(err, res)
+      else sockets.emit('update-queue', { queue, id })
+    })
+  })
+}
